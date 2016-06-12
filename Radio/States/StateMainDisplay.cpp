2f987810d3c6bbeb43325f6bd499d09d44e183d1
SaiJ <christoph@christophfriedrich.de>
2016-06-12 15:42:11 +0200

    Changed directory structure

--- Radio/States/StateMainDisplay.cpp
+++ Radio/States/StateMainDisplay.cpp
@@ -1,7 +1,9 @@
 #include "StateMainDisplay.h"
 #include "DisplayHandler.h"
+#include "Debug.h"
 
 void StateMainDisplay::onEnter() {
+  debugPrintf(F("Entering Main Display State"));
   DisplayHandler::clearDisplay();
 }
 

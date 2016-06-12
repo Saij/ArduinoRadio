ee9d3d835f59995de6b4582eef5a0050ee78c131
SaiJ <christoph@christophfriedrich.de>
2016-06-12 11:17:29 +0200

    Test

--- Radio/Radio.ino
+++ Radio/Radio.ino
@@ -1,24 +1,14 @@
 #define DEBUG
 
-// Buttons
 #include "ButtonHandler.h"
-#include "Button.h"
-
-// States
 #include "StateHandler.h"
 
 void setup() {
-  // Setup buttons
   ButtonHandler::setupButtons();
-
-  // Setup states
   StateHandler::setupStates();
 }
 
 void loop() { 
-  // Update buttons
   ButtonHandler::updateButtons();
-
-  // Update states
   StateHandler::updateStates();
 }
